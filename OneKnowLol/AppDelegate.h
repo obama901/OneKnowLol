c86d4e311624977692e8a22653dfe2b1efecb82c
Ardee <goobama@outlook.com>
2016-08-23 15:34:06 +0800

    Initial Commit

---
+++ OneKnowLol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneKnowLol
+//
+//  Created by Ardee on 16/8/23.
+//  Copyright © 2016年 Ardee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneKnowLol/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OneKnowLol
+//
+//  Created by Ardee on 16/8/23.
+//  Copyright © 2016年 Ardee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
